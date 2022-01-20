cf0908dcf8dba7e3362617fc85a62e27c364a290
gekoparty <gekoparty@hotmail.com>
2022-01-20 21:50:33 +0100

    Async merc

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,6 +3,10 @@ import Async from "./Async";
 
 describe('Async component', () => {
     test('Renders component if test succeeds', async () => {
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: async () => [{id: 1, title: 'first post'}],
+        });
         render(<Async />)
 
         const listItemElements = await screen.findAllByRole('listitem');
