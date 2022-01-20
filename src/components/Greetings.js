8c018317719f516999f698578523a1993bcf70b5
gekoparty <gekoparty@hotmail.com>
2022-01-20 13:40:58 +0100

    testing button

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 const Greetings = () => {
+
+    const [changedText, setChangedText] = useState(false);
+
+    const changeTextHandler = () => {
+        setChangedText(true);
+    }
+
   return (
     <div>
       <h2>Hello world</h2>
-      <p>Greetings</p>
+      {!changedText && <p>Greetings</p>} 
+      {/* <p>Greetings</p> */}
+      {changedText && <p>Changed Text</p>}
+      <button onClick={changeTextHandler}>Change Text</button>
     </div>
   );
 };
--- src/components/Greetings.test.js
+++ src/components/Greetings.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greetings from "./Greetings";
 
 describe("Greeting component", () => {
@@ -12,5 +13,37 @@ describe("Greeting component", () => {
     // Assert
     const helloworldelement = screen.getByText("Hello World", { exact: false });
     expect(helloworldelement).toBeInTheDocument();
+  });
+
+  test("renders greetings if the button was NOT clicked", () => {
+    render(<Greetings />);
+
+    const outpoutElement = screen.getByText("Greetings", { exact: false });
+    expect(outpoutElement).toBeInTheDocument();
+  });
+
+  test("renders changed text if button was clicked", () => {
+    //Arrange
+    render(<Greetings />);
+
+    //Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    //Assert
+    const outpoutElement = screen.getByText("changed text", { exact: false });
+    expect(outpoutElement).toBeInTheDocument();
+  });
+
+  test('Greeting component does not render "Greetings" if button was clicked', () => {
+       render(<Greetings />);
+
+    //Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    //Assert
+    const outpoutElement = screen.queryByText("Greetings", { exact: false });
+    expect(outpoutElement).toBeNull();
   });
 });
